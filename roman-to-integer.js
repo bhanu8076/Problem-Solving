f74004ddf0f4aca2a811a58f884d86785d57eb57
SOODEN <114600@incometax.net>
2023-05-14 14:05:01 +0530

    convert roman to integer using js

---
+++ roman-to-integer.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const symbols = {
+    "I": 1,
+    "V": 5,
+    "X": 10,
+    "L": 50,
+    "C": 100,
+    "D": 500,
+    "M": 1000
+    };
+    value = 0;
+    for(let i = 0; i < s.length; i+=1){
+        symbols[s[i]] < symbols[s[i+1]] ? value -= symbols[s[i]]: value +=      symbols[s[i]]
+    }
+    return value
+};
+
+let sampleTestCaseObj = {
+    firstCase: 'III',
+    secondCase: 'LVIII',
+    thirdCase: 'MCMXCIV'
+};
+
+console.log(romanToInt(sampleTestCaseObj.firstCase));
+console.log(romanToInt(sampleTestCaseObj.secondCase));
+console.log(romanToInt(sampleTestCaseObj.thirdCase));
